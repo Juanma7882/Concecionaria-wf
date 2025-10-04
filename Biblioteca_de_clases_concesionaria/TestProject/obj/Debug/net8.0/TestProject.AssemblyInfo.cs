a45e698c621e45ff09a1481e6d9902708535f39b
Juanma7882 <juanmaf236@gmail.com>
2025-10-04 09:43:13 -0300

    actualizacion

--- Biblioteca_de_clases_concesionaria/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Biblioteca_de_clases_concesionaria/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d635f45c0cb7aba45f9462e859f1f477610931c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
